f6687de4baee1830d52f853f5939a3d4fe8a97ea
Jairo Rafael Rojas Cribillero <jairo@MacBook-Pro-de-Jairo.local>
2019-12-18 13:57:37 -0500

    feat(curriculum): refactor .htaccess

---
+++ app/build/precache-manifest.9752145e6e6f7dc7cb3fbc9009533248.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f24d58f3aaa8a0d899d3a51259699ac6",
+    "url": "/app/build/index.html"
+  },
+  {
+    "revision": "42ee454ab25d76c7a1e3",
+    "url": "/app/build/static/css/main.50f43438.chunk.css"
+  },
+  {
+    "revision": "293bf571e665edb60c1f",
+    "url": "/app/build/static/js/2.709881c0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/app/build/static/js/2.709881c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "42ee454ab25d76c7a1e3",
+    "url": "/app/build/static/js/main.673349b1.chunk.js"
+  },
+  {
+    "revision": "2e801c18fd85dfcd5b98",
+    "url": "/app/build/static/js/runtime-main.1309151d.js"
+  },
+  {
+    "revision": "2aa85eff1272e48b79ed2b4f95be2719",
+    "url": "/app/build/static/media/1.2aa85eff.jpg"
+  },
+  {
+    "revision": "ab1682e75c245e1209b2abb0091cb738",
+    "url": "/app/build/static/media/1.ab1682e7.jpeg"
+  },
+  {
+    "revision": "b03672fe9bc3bdb0af7ff7325f68099c",
+    "url": "/app/build/static/media/1.b03672fe.jpeg"
+  },
+  {
+    "revision": "c978969d6da6d47873cfc064bee588fe",
+    "url": "/app/build/static/media/1.c978969d.jpg"
+  },
+  {
+    "revision": "fd5efe4e899c0d3079c35150b5239ea7",
+    "url": "/app/build/static/media/1.fd5efe4e.jpg"
+  },
+  {
+    "revision": "4efd23c71530e8fba60f5fece76d1658",
+    "url": "/app/build/static/media/10.4efd23c7.jpg"
+  },
+  {
+    "revision": "21aa2b8a7968e65473fe689e67dc0cd5",
+    "url": "/app/build/static/media/2.21aa2b8a.jpg"
+  },
+  {
+    "revision": "9ab9b52cc48c1d74fddb028e36ba50a9",
+    "url": "/app/build/static/media/2.9ab9b52c.jpg"
+  },
+  {
+    "revision": "c0b99f92f2a81f03036a83c6c2a4f3a6",
+    "url": "/app/build/static/media/2.c0b99f92.jpg"
+  },
+  {
+    "revision": "7e665ab6c7678eedc7b2a73add90fa12",
+    "url": "/app/build/static/media/3.7e665ab6.jpg"
+  },
+  {
+    "revision": "8648d985e6918181038c899a40c6b441",
+    "url": "/app/build/static/media/3.8648d985.jpg"
+  },
+  {
+    "revision": "f1463f36ef7125c9e519206c33d826ff",
+    "url": "/app/build/static/media/3.f1463f36.jpg"
+  },
+  {
+    "revision": "44f7469203b909057158200c848fb6bd",
+    "url": "/app/build/static/media/4.44f74692.jpg"
+  },
+  {
+    "revision": "b5208f6b5eae010ac98ca46d6c2b5f15",
+    "url": "/app/build/static/media/4.b5208f6b.jpg"
+  },
+  {
+    "revision": "63de43e906eb8247d9110c5793e5056d",
+    "url": "/app/build/static/media/5.63de43e9.jpg"
+  },
+  {
+    "revision": "a071686825112cdaf29d2d74b19c42d8",
+    "url": "/app/build/static/media/5.a0716868.jpg"
+  },
+  {
+    "revision": "d5f9636a0b8212a00ca3e5e757fa0098",
+    "url": "/app/build/static/media/5.d5f9636a.jpg"
+  },
+  {
+    "revision": "f858b7a71ea2762c6e64a006ff170270",
+    "url": "/app/build/static/media/6.f858b7a7.jpg"
+  },
+  {
+    "revision": "f68773baa45a9c35177ec627d83a5437",
+    "url": "/app/build/static/media/7.f68773ba.jpg"
+  },
+  {
+    "revision": "36fae6e3b38d413e6e8a565f7327a6ff",
+    "url": "/app/build/static/media/9.36fae6e3.jpg"
+  },
+  {
+    "revision": "2e90d5152ce92858b62ba053c7b9d2cb",
+    "url": "/app/build/static/media/OpenSans-Bold-webfont.2e90d515.woff"
+  },
+  {
+    "revision": "76cc6be5d8a231dc012fef4bdb86f79c",
+    "url": "/app/build/static/media/OpenSans-Bold-webfont.76cc6be5.ttf"
+  },
+  {
+    "revision": "de7ef31e6295902347c5c3643b2d82da",
+    "url": "/app/build/static/media/OpenSans-Italic-webfont.de7ef31e.ttf"
+  },
+  {
+    "revision": "f42641eed834f7b97a9499362c6c8855",
+    "url": "/app/build/static/media/OpenSans-Italic-webfont.f42641ee.woff"
+  },
+  {
+    "revision": "2e98fc3ce85f31f63010b706259cb604",
+    "url": "/app/build/static/media/OpenSans-Light-webfont.2e98fc3c.ttf"
+  },
+  {
+    "revision": "45b47f3e9c7d74b80f5c6e0a3c513b23",
+    "url": "/app/build/static/media/OpenSans-Light-webfont.45b47f3e.woff"
+  },
+  {
+    "revision": "1d22953c479914c2f801e08de666b0e8",
+    "url": "/app/build/static/media/OpenSans-LightItalic-webfont.1d22953c.ttf"
+  },
+  {
+    "revision": "b553da506077488bc65289e10841d527",
+    "url": "/app/build/static/media/OpenSans-LightItalic-webfont.b553da50.woff"
+  },
+  {
+    "revision": "488d5cc145299ba07b75495100419ee6",
+    "url": "/app/build/static/media/OpenSans-Regular-webfont.488d5cc1.ttf"
+  },
+  {
+    "revision": "79515ad0788973c533405f7012dfeccd",
+    "url": "/app/build/static/media/OpenSans-Regular-webfont.79515ad0.woff"
+  },
+  {
+    "revision": "697574b47bcfdd2c45e3e63c7380dd67",
+    "url": "/app/build/static/media/OpenSans-Semibold-webfont.697574b4.woff"
+  },
+  {
+    "revision": "b32acea6fd3c228b5059042c7ad21c55",
+    "url": "/app/build/static/media/OpenSans-Semibold-webfont.b32acea6.ttf"
+  },
+  {
+    "revision": "857f3f63450c84e95ccf8b436557a8f3",
+    "url": "/app/build/static/media/cv.857f3f63.pdf"
+  },
+  {
+    "revision": "26ec3c7d0366e0825d705c6e224a8803",
+    "url": "/app/build/static/media/et-line.26ec3c7d.eot"
+  },
+  {
+    "revision": "909c9aa54f34c3a1da6f98d37098a560",
+    "url": "/app/build/static/media/et-line.909c9aa5.svg"
+  },
+  {
+    "revision": "98126e3e1238b0f3b941ad285320ce28",
+    "url": "/app/build/static/media/et-line.98126e3e.ttf"
+  },
+  {
+    "revision": "b01ff252761958325faab1535c90c87f",
+    "url": "/app/build/static/media/et-line.b01ff252.woff"
+  },
+  {
+    "revision": "26ec3c7d0366e0825d705c6e224a8803",
+    "url": "/app/build/static/media/et-lined41d.26ec3c7d.eot"
+  },
+  {
+    "revision": "279c529f5e8b14bf637ef860f23128de",
+    "url": "/app/build/static/media/fontello41e0.279c529f.eot"
+  },
+  {
+    "revision": "3ba036745a7c45c76ef865e30fe26118",
+    "url": "/app/build/static/media/fontello41e0.3ba03674.woff"
+  },
+  {
+    "revision": "783226d46266680c8404e1d3875f23ee",
+    "url": "/app/build/static/media/fontello41e0.783226d4.svg"
+  },
+  {
+    "revision": "df4d269e1b45128549da152fca38bcf8",
+    "url": "/app/build/static/media/fontello41e0.df4d269e.ttf"
+  },
+  {
+    "revision": "557857afe0a3cc7361c5b81d9329dd8e",
+    "url": "/app/build/static/media/img-service.557857af.jpg"
+  },
+  {
+    "revision": "9c89d21a92ddba1f65deef92eeb2f9d3",
+    "url": "/app/build/static/media/titillium-bold-webfont.9c89d21a.ttf"
+  },
+  {
+    "revision": "c4a20bba5a80df48bfadca4ff8a47f7b",
+    "url": "/app/build/static/media/titillium-bold-webfont.c4a20bba.woff"
+  },
+  {
+    "revision": "230bd3d60764bd39af1d81a1ee842a5a",
+    "url": "/app/build/static/media/titillium-lightupright-webfont.230bd3d6.ttf"
+  },
+  {
+    "revision": "3d504cb0f073087319084c6f0e243907",
+    "url": "/app/build/static/media/titillium-lightupright-webfont.3d504cb0.woff"
+  },
+  {
+    "revision": "356cd49f5965fbdc6c6f0aa5d2146aa9",
+    "url": "/app/build/static/media/titillium-regularupright-webfont.356cd49f.eot"
+  },
+  {
+    "revision": "b68b17cae63d95376fd29eb1f8a6f9f4",
+    "url": "/app/build/static/media/titillium-regularupright-webfont.b68b17ca.woff"
+  },
+  {
+    "revision": "dc4578e5a8e692ee32c756f09764609b",
+    "url": "/app/build/static/media/titillium-regularupright-webfont.dc4578e5.ttf"
+  },
+  {
+    "revision": "356cd49f5965fbdc6c6f0aa5d2146aa9",
+    "url": "/app/build/static/media/titillium-regularupright-webfontd41d.356cd49f.eot"
+  },
+  {
+    "revision": "18ce679c5391b6ff2f22a8488ad63c6d",
+    "url": "/app/build/static/media/titillium-semibold-webfont.18ce679c.woff"
+  },
+  {
+    "revision": "2b9eb175dd6c0970743aea1096047ed6",
+    "url": "/app/build/static/media/titillium-semibold-webfont.2b9eb175.eot"
+  },
+  {
+    "revision": "dec19f45aa1a300f641b003be0a5b1b7",
+    "url": "/app/build/static/media/titillium-semibold-webfont.dec19f45.svg"
+  },
+  {
+    "revision": "f78f471ae2a1c34dd0cba4ec4a47cd3c",
+    "url": "/app/build/static/media/titillium-semibold-webfont.f78f471a.ttf"
+  },
+  {
+    "revision": "2b9eb175dd6c0970743aea1096047ed6",
+    "url": "/app/build/static/media/titillium-semibold-webfontd41d.2b9eb175.eot"
+  },
+  {
+    "revision": "412d939c9092b24b5f4733a3d427eede",
+    "url": "/app/build/static/media/titillium-thinupright-webfont.412d939c.woff"
+  },
+  {
+    "revision": "8d0a7bccd5dff27e2a852d56adba5a1f",
+    "url": "/app/build/static/media/titillium-thinupright-webfont.8d0a7bcc.eot"
+  },
+  {
+    "revision": "af2bf5a32e2e0dffdf014788ec80a81b",
+    "url": "/app/build/static/media/titillium-thinupright-webfont.af2bf5a3.ttf"
+  },
+  {
+    "revision": "8d0a7bccd5dff27e2a852d56adba5a1f",
+    "url": "/app/build/static/media/titillium-thinupright-webfontd41d.8d0a7bcc.eot"
+  }
+]);
